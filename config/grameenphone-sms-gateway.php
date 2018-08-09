46cf2f89210e25391cd7df3e58c7d8f13872ee2d
emtiazzahid <zahid@nextdot.com.au>
2018-08-09 16:07:37 +0600

    config dummy values removed

--- config/grameenphone-sms-gateway.php
+++ config/grameenphone-sms-gateway.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'username'  => 'ORKOAdmin',
-    'password' => 'ORKOAdmin123',
-    'apicode'  => '1',
-    'countrycode'  => '880',
-    'cli'  => 'ORKO',
-    'messagetype'  => '1',
-    'messageid'  => '0'
+    'username'  => '',
+    'password' => '',
+    'apicode'  => '',
+    'countrycode'  => '',
+    'cli'  => '',
+    'messagetype'  => '',
+    'messageid'  => ''
 ];
